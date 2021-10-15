c0251a939cfe19c95670b62fff204147bf697b32
Hasnatul Jilan <>
2021-10-16 04:07:56 +0600

    All but the final dialogue

--- UI/board.py
+++ UI/board.py
@@ -1,60 +1,199 @@
 import pygame, sys
 import numpy as np
 
-from UI.environment import initializeBoard
+from UI.environment import initializeBoard, doReset, recreateEnv, updateHunter, wumpus_isDead
 
 pygame.init()
-screenHeight = 600
-screenWidth = 800
-
+screenHeight = 650
+screenWidth = 1100
+LgCOLR= (255,255,255)
 screen = pygame.display.set_mode((screenWidth, screenHeight))
 pygame.display.set_caption('Wumpus World')
-screen.fill((240, 175, 10))
+wum_pic = pygame.image.load("./UI/assets/wumpus.jpg")
+hunter_pic = pygame.image.load("./UI/assets/hunter.jpg")
+treasure_pic = pygame.image.load("./UI/assets/treasure.jpg")
+pit_pic = pygame.image.load("./UI/assets/pit.png")
+screen.fill((5, 48, 0))
+pygame.draw.rect(screen, (1, 5, 0), [50, 10, 600, 600])
+X_COORD = 52
+Y_COORD = 552
+Hunter_POS = 90
+arrow = 1
+
+begin_button = pygame.draw.rect(screen, (157, 252, 3), (790, 550, 70, 35));
+reset_button = pygame.draw.rect(screen, (252, 215, 3), (885, 550, 70, 35));
+font = pygame.font.SysFont('freesans', 18)
+lbl_begin = font.render('Begin', True, (5, 48, 0))
+lbl_reset = font.render('Reset', True, (5, 48, 0))
+screen.blit(lbl_begin, (806, 557))
+screen.blit(lbl_reset, (901, 557))
 
 
 def makeTiles():
 
-    lineClr = (54, 40, 5)
+    boxClr = (54, 40, 5)
+    lineClr = (252, 59, 0)
 
-    pygame.draw.line(screen,lineClr,(50,50),(550,50),5)
-    pygame.draw.line(screen, lineClr, (50, 550), (550, 550), 5)
-    pygame.draw.line(screen, lineClr, (50, 50), (50, 550), 5)
-    pygame.draw.line(screen, lineClr, (550, 50), (550, 550), 5)
+    pygame.draw.line(screen,boxClr,(50,10),(650,10),5)
+    pygame.draw.line(screen, boxClr, (50, 610), (650, 610), 5)
+    pygame.draw.line(screen, boxClr, (50, 10), (50, 610), 5)
+    pygame.draw.line(screen, boxClr, (650, 10), (650, 610), 5)
 
-    pygame.draw.line(screen, lineClr, (50, 100), (550, 100), 2)
-    pygame.draw.line(screen, lineClr, (50, 150), (550, 150), 2)
-    pygame.draw.line(screen, lineClr, (50, 200), (550, 200), 2)
-    pygame.draw.line(screen, lineClr, (50, 250), (550, 250), 2)
-    pygame.draw.line(screen, lineClr, (50, 300), (550, 300), 2)
-    pygame.draw.line(screen, lineClr, (50, 350), (550, 350), 2)
-    pygame.draw.line(screen, lineClr, (50, 400), (550, 400), 2)
-    pygame.draw.line(screen, lineClr, (50, 450), (550, 450), 2)
-    pygame.draw.line(screen, lineClr, (50, 500), (550, 500), 2)
+    pygame.draw.line(screen, lineClr, (50, 70), (650, 70), 2)
+    pygame.draw.line(screen, lineClr, (50, 130), (650, 130), 2)
+    pygame.draw.line(screen, lineClr, (50, 190), (650, 190), 2)
+    pygame.draw.line(screen, lineClr, (50, 250), (650, 250), 2)
+    pygame.draw.line(screen, lineClr, (50, 310), (650, 310), 2)
+    pygame.draw.line(screen, lineClr, (50, 370), (650, 370), 2)
+    pygame.draw.line(screen, lineClr, (50, 430), (650, 430), 2)
+    pygame.draw.line(screen, lineClr, (50, 490), (650, 490), 2)
+    pygame.draw.line(screen, lineClr, (50, 550), (650, 550), 2)
 
-    pygame.draw.line(screen, lineClr, (100, 50), (100, 550), 2)
-    pygame.draw.line(screen, lineClr, (150, 50), (150, 550), 2)
-    pygame.draw.line(screen, lineClr, (200, 50), (200, 550), 2)
-    pygame.draw.line(screen, lineClr, (250, 50), (250, 550), 2)
-    pygame.draw.line(screen, lineClr, (300, 50), (300, 550), 2)
-    pygame.draw.line(screen, lineClr, (350, 50), (350, 550), 2)
-    pygame.draw.line(screen, lineClr, (400, 50), (400, 550), 2)
-    pygame.draw.line(screen, lineClr, (450, 50), (450, 550), 2)
-    pygame.draw.line(screen, lineClr, (500, 50), (500, 550), 2)
+    pygame.draw.line(screen, lineClr, (110, 10), (110, 610), 2)
+    pygame.draw.line(screen, lineClr, (170, 10), (170, 610), 2)
+    pygame.draw.line(screen, lineClr, (230, 10), (230, 610), 2)
+    pygame.draw.line(screen, lineClr, (290, 10), (290, 610), 2)
+    pygame.draw.line(screen, lineClr, (350, 10), (350, 610), 2)
+    pygame.draw.line(screen, lineClr, (410, 10), (410, 610), 2)
+    pygame.draw.line(screen, lineClr, (470, 10), (470, 610), 2)
+    pygame.draw.line(screen, lineClr, (530, 10), (530, 610), 2)
+    pygame.draw.line(screen, lineClr, (590, 10), (590, 610), 2)
 
-    
-def init():
+    pygame.display.update()
+
+def setLegends():
+
+    global wum_pic,hunter_pic,treasure_pic,pit_pic
+
+    wum_pic = pygame.transform.scale(wum_pic, (40, 40))
+    hunter_pic = pygame.transform.scale(hunter_pic, (40, 40))
+    treasure_pic = pygame.transform.scale(treasure_pic, (40, 40))
+    pit_pic = pygame.transform.scale(pit_pic, (40, 40))
+    screen.blit(wum_pic, (700, 10))
+    screen.blit(hunter_pic, (700, 55))
+    screen.blit(treasure_pic, (700, 100))
+    screen.blit(pit_pic, (700, 145))
+    pygame.draw.circle(screen, (232, 15, 0), (720,210), 10)
+    pygame.draw.circle(screen, (252, 193, 1), (720, 240), 10)
+    pygame.draw.circle(screen, (29, 182, 224), (720, 270), 10)
+    pygame.draw.circle(screen, (66, 230, 7), (720, 300), 10)
+
+    font = pygame.font.SysFont('freesans', 15)
+
+    legend_wumpus = font.render('Wumpus', True, LgCOLR)
+    legend_hunter = font.render('Hunter', True, LgCOLR)
+    legend_pit = font.render('Pit', True, LgCOLR)
+    legend_treasure = font.render('Treasure', True, LgCOLR)
+    legend_stench = font.render('Stench', True, LgCOLR)
+    legend_breeze = font.render('Breeze', True, LgCOLR)
+    legend_glitter = font.render('Glitter', True, LgCOLR)
+    legend_safe = font.render('Safe', True, LgCOLR)
+
+    screen.blit(legend_wumpus, (775, 20))
+    screen.blit(legend_hunter, (775, 65))
+    screen.blit(legend_treasure, (775, 110))
+    screen.blit(legend_pit, (775, 155))
+    screen.blit(legend_stench, (775, 200))
+    screen.blit(legend_glitter, (775, 231))
+    screen.blit(legend_breeze, (775, 261))
+    screen.blit(legend_safe, (775, 291))
+
+    pygame.draw.rect(screen, (0, 2, 18), [935, 15, 115, 40])
+    font = pygame.font.SysFont('freesans', 20)
+    lbl_arrow = font.render('Arrow: ', True, (254,0,5))
+    lbl_acont = font.render(str(arrow), True, (254, 0, 5))
+    screen.blit(lbl_arrow, (960, 22))
+    screen.blit(lbl_acont, (1010, 22))
+
+def originFill():
+
+    screen.fill((5, 48, 0))
+    pygame.draw.rect(screen, (1, 5, 0), [50, 10, 600, 600])
+    begin_button = pygame.draw.rect(screen, (157, 252, 3), (790, 550, 70, 35));
+    reset_button = pygame.draw.rect(screen, (252, 215, 3), (885, 550, 70, 35));
+    font = pygame.font.SysFont('freesans', 18)
+    lbl_begin = font.render('Begin', True, (5, 48, 0))
+    lbl_reset = font.render('Reset', True, (5, 48, 0))
+    screen.blit(lbl_begin, (806, 557))
+    screen.blit(lbl_reset, (901, 557))
+
     makeTiles()
-    initializeBoard()
+    setLegends()
+def gameOverDialogue(result):
+    pygame.draw.rect(screen, (1, 5, 0), [100, 300, 300, 100])
+
+def init():
+
+    global X_COORD,Y_COORD,Hunter_POS,arrow
+    originFill()
+    initializeBoard(screen)
+
     while True:
         pygame.display.update()
         for event in pygame.event.get():
+            pygame.display.update()
             if event.type == pygame.QUIT:
                 sys.exit(0)
-            if event.type ==pygame.MOUSEBUTTONDOWN:
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    if Hunter_POS-10>=0:
+                        Hunter_POS-=10
+                        originFill()
+                        recreateEnv(screen)
+                        b = updateHunter(Hunter_POS+10,Hunter_POS,screen)
+                        if b !=None:
+                            gameOverDialogue(b)
+
+                if event.key == pygame.K_DOWN:
+                    if Hunter_POS+10<100:
+                        Hunter_POS+=10
+                        originFill()
+                        recreateEnv(screen)
+                        b = updateHunter(Hunter_POS-10,Hunter_POS,screen)
+
+                if event.key == pygame.K_LEFT:
+                    if Hunter_POS-1>=0:
+                        Hunter_POS-=1
+                        originFill()
+                        recreateEnv(screen)
+                        b = updateHunter(Hunter_POS-1,Hunter_POS,screen)
+
+                if event.key == pygame.K_RIGHT:
+                    if Hunter_POS+1<100:
+                        Hunter_POS+=1
+                        originFill()
+                        recreateEnv(screen)
+                        b = updateHunter(Hunter_POS+1,Hunter_POS,screen)
+
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+
+                click = event.pos
+
+                if reset_button.collidepoint(click):
+                    X_COORD = 52
+                    Y_COORD = 552
+                    Hunter_POS = 90
+                    arrow = 1
+                    doReset()
+                    originFill()
+                    initializeBoard(screen)
+
+                if begin_button.collidepoint(click):
+                    wumpus_isDead(screen)
+                    arrow-=1
+                    originFill()
+                    recreateEnv(screen)
+                    updateHunter(Hunter_POS, Hunter_POS, screen)
+
+            '''if event.type ==pygame.MOUSEBUTTONDOWN:
                 coordX = event.pos[0]
                 coordY = event.pos[1]
-
-                row = (coordY // 50)-1;
-                col = (coordX // 50) - 1;
+                coordY-=10
+                coordX-=50
+                row = (coordY // 60);
+                col = (coordX // 60);
 
                 print(row, col)
+                #pygame.draw.circle(screen, (0, 255, 0), (600, 500), 15)'''
--- UI/environment.py
+++ UI/environment.py
@@ -1,80 +1,274 @@
 import numpy as np
 import random
-player_pos,safe,treasury,pit,wumpus, breeze, glitter, stench, max_pit,board_size = "player","safe","gold","pit","wumpus","breeze","glitter","stench",10,10
+import pygame
+
+hunter_pos,safe,treasure,pit,wumpus, breeze, glitter, stench, max_pit,board_size = "hunter","safe","treasure","pit","wumpus","breeze","glitter","stench",10,10
 arrow = True
+wum_pos =0
+
+def setPic(wp,pp,tp,hp):
+
+    wum_pic = wp
+    pit_pic = pp
+    treasure_pic = tp
+    hunter_pic = hp
+
+wum_pic = pygame.image.load("./UI/assets/wumpus.jpg")
+wum_pic = pygame.transform.scale(wum_pic, (58, 58))
+wumDead_pic = pygame.image.load("./UI/assets/dead_wumpus.png")
+wumDead_pic = pygame.transform.scale(wumDead_pic, (58, 58))
+pit_pic = pygame.image.load("./UI/assets/pit.png")
+pit_pic = pygame.transform.scale(pit_pic, (58, 58))
+treasure_pic = pygame.image.load("./UI/assets/treasure.jpg")
+treasure_pic = pygame.transform.scale(treasure_pic, (58, 58))
+hunter_pic = pygame.image.load("./UI/assets/hunter.jpg")
+hunter_pic = pygame.transform.scale(hunter_pic, (58, 58))
+hunter_pic_D =  pygame.transform.rotate(hunter_pic,270)
+hunter_pic_L =  pygame.transform.flip(hunter_pic,True,False)
+hunter_pic_U =  pygame.transform.rotate(hunter_pic,90)
+hunter_pic_R =  hunter_pic
+
 #board = [[]]
-board = np.empty(100, dtype=np.object)
-for i in range(0,100):
+board = np.empty(101, dtype=np.object)
+for i in range(0,101):
     board[i] = []
     board[i].append(i)
 
-def setWumpus():
+def checkEmpty(list_pos,direction):
 
+    if direction == "X":
+        if all ([board[list_pos].count("pit")==0,
+             board[list_pos].count("treasure")==0,board[list_pos].count("wumpus")==0]):
+            return True
+        return False
+
+    if direction == "E":
+        if all ([board[list_pos + 1].count("pit")==0, board[list_pos + 1].count("hunter")==0,
+             board[list_pos + 1].count("treasure")==0,board[list_pos + 1].count("wumpus")==0]):
+            return True
+        return False
+    if direction == "W":
+        if all([board[list_pos - 1].count("pit") == 0, board[list_pos - 1].count("hunter") == 0,
+                board[list_pos - 1].count("treasure") == 0, board[list_pos - 1].count("wumpus") == 0]):
+            return True
+        return False
+    if direction == "N":
+        if all([board[list_pos - 10].count("pit") == 0, board[list_pos - 10].count("hunter") == 0,
+                board[list_pos - 10].count("treasure") == 0, board[list_pos - 10].count("wumpus") == 0]):
+            return True
+        return False
+    if direction == "S":
+        if all([board[list_pos + 10].count("pit") == 0, board[list_pos + 10].count("hunter") == 0,
+                board[list_pos + 10].count("treasure") == 0, board[list_pos + 10].count("wumpus") == 0]):
+            return True
+        return False
+
+
+def setWumpus(screen,wum_pic):
+    global wum_pos
+    wum_pic = pygame.transform.scale(wum_pic, (58, 58))
     rando_row = random.randint(0, 9)
     rando_col = random.randint(0, 9)
 
     list_pos = rando_row*10+rando_col
     if list_pos == 90:
-        setWumpus()
+        setWumpus(screen,wum_pic)
 
     else:
         board[list_pos].append(wumpus)
+        wum_pos = list_pos
+        screen.blit(wum_pic, (((rando_col*60+52),(rando_row*60)+12)))
+
         if rando_col + 1 < 10:
             board[list_pos + 1].append(stench)
+            if checkEmpty(list_pos, "E"):
+                pygame.draw.circle(screen, (232, 15, 0), (((rando_col+1)*60+52+12), ((rando_row)*60+12+12)), 10)
         if rando_col - 1 >= 0:
             board[list_pos - 1].append(stench)
+            if checkEmpty(list_pos, "W"):
+                pygame.draw.circle(screen, (232, 15, 0), (((rando_col - 1) * 60 + 52 + 12), ((rando_row) * 60 + 12 + 12)), 10)
         if rando_row + 1 < 10:
             board[list_pos + 10].append(stench)
+            if checkEmpty(list_pos, "S"):
+                pygame.draw.circle(screen, (232, 15, 0), (((rando_col) * 60 + 52 + 12), ((rando_row+1) * 60 + 12 + 12)),
+                               10)
         if rando_row - 1 >= 0:
             board[list_pos - 10].append(stench)
+            if checkEmpty(list_pos, "N"):
+                pygame.draw.circle(screen, (232, 15, 0), (((rando_col) * 60 + 52 + 12), ((rando_row-1) * 60 + 12 + 12)),
+                               10)
 
-def setPits():
+def setPits(screen,pit_pic):
+    pit_pic = pygame.transform.scale(pit_pic, (58, 58))
+    x=0
+    while (x<max_pit):
 
-    for x in range(0,max_pit):
         rando_row = random.randint(0, 9)
         rando_col = random.randint(0, 9)
 
         list_pos = rando_row * 10 + rando_col
 
-        if list_pos == 90 or board[list_pos].count("wumpus")>0 or board[list_pos].count("pit")>0:
-            x-=1
+        if list_pos == 90 or board[list_pos].count("wumpus")>0 or board[list_pos].count("pit")>0 or board[list_pos].count("treasure")>0:
+            continue
+
         else:
+            x+=1
             board[list_pos].append(pit)
+            screen.blit(pit_pic, (((rando_col * 60 + 52), (rando_row * 60) + 12)))
+
             if rando_col + 1 < 10 and board[list_pos+1].count("breeze")==0:
                 board[list_pos+1].append(breeze)
+                if checkEmpty(list_pos,"E"):
+                    pygame.draw.circle(screen, (29, 182, 224),
+                                   (((rando_col + 1) * 60 + 52 + 28), ((rando_row) * 60 + 12 + 28)), 10)
+
             if rando_col - 1 >= 0 and board[list_pos-1].count("breeze")==0:
                 board[list_pos-1].append(breeze)
+                if checkEmpty(list_pos,"W"):
+                    pygame.draw.circle(screen, (29, 182, 224),
+                                   (((rando_col - 1) * 60 + 52 + 28), ((rando_row) * 60 + 12 + 28)), 10)
+
             if rando_row + 1 < 10 and board[list_pos+10].count("breeze")==0:
                 board[list_pos+10].append(breeze)
+                if checkEmpty(list_pos, "S"):
+                    pygame.draw.circle(screen, (29, 182, 224),
+                                   (((rando_col) * 60 + 52 + 28), ((rando_row+1) * 60 + 12 + 28)), 10)
+
             if rando_row - 1 >= 0 and board[list_pos-10].count("breeze")==0:
                 board[list_pos-10].append(breeze)
+                if checkEmpty(list_pos, "N"):
+                    pygame.draw.circle(screen, (29, 182, 224),
+                                   (((rando_col) * 60 + 52 + 28), ((rando_row-1) * 60 + 12 + 28)), 10)
 
-def setTreasury():
+
+def setTreasury(screen,treasure_pic):
+    treasure_pic = pygame.transform.scale(treasure_pic, (58, 58))
     rando_row = random.randint(0, 9)
     rando_col = random.randint(0, 9)
 
     list_pos = rando_row * 10 + rando_col
 
     if list_pos== 90 or board[list_pos].count("wumpus")>0 or board[list_pos].count("pit")>0:
-        setTreasury()
+        setTreasury(screen,treasure_pic)
     else:
-        board[list_pos].append(treasury)
+        board[list_pos].append(treasure)
+        screen.blit(treasure_pic, (((rando_col * 60 + 52), (rando_row * 60) + 12)))
+
         if rando_col + 1 < 10:
             board[list_pos + 1].append(glitter)
+            if checkEmpty(list_pos, "E"):
+                pygame.draw.circle(screen, (252, 193, 1),
+                               (((rando_col + 1) * 60 + 52 + 44), ((rando_row) * 60 + 12 + 44)), 10)
+
         if rando_col - 1 >= 0:
             board[list_pos - 1].append(glitter)
+            if checkEmpty(list_pos, "W"):
+                pygame.draw.circle(screen, (252, 193, 1),
+                               (((rando_col - 1) * 60 + 52 + 44), ((rando_row) * 60 + 12 + 44)), 10)
+
         if rando_row + 1 < 10:
             board[list_pos + 10].append(glitter)
+            if checkEmpty(list_pos, "S"):
+                pygame.draw.circle(screen, (252, 193, 1),
+                               (((rando_col) * 60 + 52 + 44), ((rando_row+1) * 60 + 12 + 44)), 10)
+
         if rando_row - 1 >= 0:
             board[list_pos - 10].append(glitter)
+            if checkEmpty(list_pos, "N"):
+                pygame.draw.circle(screen, (252, 193, 1),
+                               (((rando_col) * 60 + 52 + 44), ((rando_row-1) * 60 + 12 + 44)), 10)
 
-def initializeBoard():
+def recreateEnv(screen):
+    global wum_pic,pit_pic,treasure_pic
+    wum_pic = pygame.transform.scale(wum_pic, (58, 58))
+    pit_pic = pygame.transform.scale(pit_pic, (58, 58))
+    treasure_pic = pygame.transform.scale(treasure_pic, (58, 58))
 
-    board[90].append(player_pos)
+    for i in range(0, 100):
+            #screen.blit(wum_pic, ((((i%10) * 60 + 52), ((i//10) * 60) + 12)))
+        if board[i].count("wumpus")>0:
+            screen.blit(wum_pic, ((((i%10) * 60 + 52), ((i//10) * 60) + 12)))
+        if board[i].count("treasure") > 0:
+            screen.blit(treasure_pic, ((((i%10) * 60 + 52), ((i//10) * 60) + 12)))
+        if board[i].count("pit") > 0:
+            screen.blit(pit_pic, ((((i%10) * 60 + 52), ((i//10) * 60) + 12)))
+        if board[i].count("glitter") > 0 and checkEmpty(i,"X"):
+            pygame.draw.circle(screen, (252, 193, 1),
+                               (((i%10) * 60 + 52 + 44), ((i//10) * 60 + 12 + 44)), 10)
+        if board[i].count("stench") > 0 and checkEmpty(i,"X"):
+            pygame.draw.circle(screen, (232, 15, 0), (((i%10) * 60 + 52 + 12), ((i//10) * 60 + 12 + 12)),
+                               10)
+        if board[i].count("breeze")>0 and checkEmpty(i,"X"):
+            pygame.draw.circle(screen, (29, 182, 224),
+                               (((i%10) * 60 + 52 + 28), ((i//10) * 60 + 12 + 28)), 10)
+        if board[i].count("safe")>0 and checkEmpty(i,"X"):
+            pygame.draw.circle(screen, (66, 230, 7),
+                               (((i%10) * 60 + 52 + 44), ((i//10) * 60 + 12 + 12)), 10)
 
-    setWumpus()
-    setTreasury()
-    setPits()
-    print(board)
+
+def initializeBoard(screen):
+    global wum_pic,hunter_pic,pit_pic,treasure_pic
+    board[90].append(hunter_pos)
+    board[90].append(safe)
+    hunter_pic = pygame.transform.scale(hunter_pic, (58, 58))
+    screen.blit(hunter_pic, (52, 552))
+
+    setWumpus(screen,wum_pic)
+    setTreasury(screen,treasure_pic)
+    setPits(screen,pit_pic)
+
+def updateHunter(prev_pos, current_pos, screen):
+    global hunter_pic
+    if board[prev_pos].count(hunter_pos)>0:
+        board[prev_pos].remove(hunter_pos)
+    board[current_pos].append(hunter_pos)
+    board[current_pos].append(safe)
+    hunter_pic = pygame.transform.scale(hunter_pic, (58, 58))
+    if prev_pos-current_pos==10:
+        hunter_pic = hunter_pic_U
+    if prev_pos-current_pos==-1:
+        hunter_pic = hunter_pic_L
+    if prev_pos-current_pos==1:
+        hunter_pic = hunter_pic_R
+    if prev_pos-current_pos==-10:
+        hunter_pic = hunter_pic_D
+    #screen.blit(hunter_pic, (52, 552))
+    screen.blit(hunter_pic,((((current_pos%10) * 60 + 52), ((current_pos//10) * 60) + 12)))
+    b = checkGameOver(current_pos)
+    return b
+
+def checkGameOver(current_pos):
+    if any(wp in board[current_pos] for wp in ("wumpus","pit")):
+        return "dead"
+    if "treasure" in board[current_pos]:
+        return "victory"
+
+def wumpus_isDead(screen):
+    global wum_pos,wum_pic
+    print(wum_pos)
+    if wum_pos+1<100 and board[wum_pos+1].count(stench)>0:
+        print('ry')
+        board[wum_pos+1].remove(stench)
+    if wum_pos-1>0 and board[wum_pos-1].count(stench)>0:
+        print('ly')
+        board[wum_pos-1].remove(stench)
+    if wum_pos+10<100 and board[wum_pos+10].count(stench)>0:
+        print('dy')
+        board[wum_pos+10].remove(stench)
+    if wum_pos-10>=0 and board[wum_pos-10].count(stench)>0:
+        print('uy')
+        board[wum_pos-10].remove(stench)
+
+    wum_pic = wumDead_pic
+    screen.blit(wum_pic, ((((wum_pos % 10) * 60 + 52), ((wum_pos // 10) * 60) + 12)))
+
+def doReset():
+    global wum_pic,hunter_pic,hunter_pic_R
+    wum_pic = pygame.image.load("./UI/assets/wumpus.jpg")
+    wum_pic = pygame.transform.scale(wum_pic, (58, 58))
+    hunter_pic = hunter_pic_R
+    for i in range(0, 101):
+        board[i] = []
+        board[i].append(i)
 
 
